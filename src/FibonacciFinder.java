fb36f5c892f2eac332455d232b4dbfb620fcdc7c
Andrey Fabricio <andrey.fas@gmail.com>
2022-01-22 16:04:50 -0300

    Documentation
    Finished the project documentation and fixed some errors in the documentation

--- src/FibonacciFinder.java
+++ src/FibonacciFinder.java
@@ -21,9 +21,9 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	    JLabel N = new JLabel("You want to generate the Fibonacci sequence up to what number?");
 	    JLabel error = new JLabel("");
 	    JLabel limit = new JLabel("Max value: 7540113804746346429");
+	    JButton btn = new JButton("Generate");
 	    JTextField userTextField = new JTextField();
 	    JTextArea fbnTextField = new JTextArea();
-	    JButton btn = new JButton("Generate");
 		
 	    FibonacciFinder()
 	    {
@@ -49,13 +49,12 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 			limit.setBounds(150,30,200,40);
 		    userTextField.setBounds(130,61,140,20);
 		    fbnTextField.setBounds(65,90,360,430);
-		    fbnTextField.setLineWrap(true);
-		    fbnTextField.setVisible(false);
-		    fbnTextField.setEditable(false);
+		    fbnTextField.setLineWrap(true); // Allows auto line breaks inside fbnTextField
+		    fbnTextField.setVisible(false); // To be visible later
+		    fbnTextField.setEditable(false); // User can copy but cannot edit the content
 		    btn.setBounds(270,60,90,20);
 		    error.setBounds(60,70,400,40);
-		    // Setting the color of the error label
-		    error.setForeground(new java.awt.Color(255,0,0));
+		    error.setForeground(new java.awt.Color(255,0,0)); // Sets the color of the error label
 		}
 		
 		public void addComponentsToContainer()
@@ -86,8 +85,8 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	    @Override
 	    public void actionPerformed(ActionEvent e) 
 	    {
-	    	// If the btn is clicked resets "Calculated PI" and "PI to the Nth decimal" and the error labels
-	    	// and calls createPI function
+	    	// If the btn is clicked resets "error" and "userTextField"
+	    	// and calls createFibonacci function
 	    	if (e.getSource() == btn || e.getSource() == userTextField) {
 	        	error.setText("");
 	        	fbnTextField.setVisible(false);
@@ -99,34 +98,45 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	
 	    private void createFibonacci() {
     		
-    		int pos = 2;
+    		int pos = 2; // Stores the position in the Fibonacci sequence
+    		
+    		// Fibonacci calculation: Fn = F(n-1) + F(n-2)
+    		// N -> User input
+    		// sec -> First term in Fibonacci calculation "F(n-1)"
+    		// last -> Second term in Fibonacci calculation "F(n-2)"
+    		// next -> Third (next) term in Fibonacci calculation "Fn"
+    		// and so on...
 	    	long N, sec, last, next;
-	    	boolean validInput = true;
+	    	
+	    	boolean validInput = true; // User input is valid as default
 	    	
 	    	N = sec = next = 0;
 	    	last = 1;
 			
-	    	String tempFbn = "0, 1";
-	    	String fbnSequence = "";
+	    	String tempFbn = "0, 1"; // Stores the generated Fibonacci sequence
+	    	String fbnSequence = ""; // Stores the final text with the Fibonacci sequence
 	    	
 	        try {
-	            N = Long.parseLong(userTextField.getText());
+	            N = Long.parseLong(userTextField.getText()); // Pass user input to Long
 	        } catch(NumberFormatException nfe) {        	
 	        	error.setText("The value \"" + userTextField.getText() +"\" is not valid");
-	        	validInput = false;
+	        	validInput = false; // Error makes input invalid
 	        }
 	        
 	        if (N < 0) {
 	        	error.setText("The value \"" + userTextField.getText() +"\" "
 	        			+ "is not a valid positive number");
-	        	validInput = false;
+	        	validInput = false; // Error makes input invalid
 	        }
 	        
 	        if (validInput) {
+	        	
+	        	// The text box is visible only if the user input is valid
 	        	fbnTextField.setVisible(true);
 	        	
 	        	if (N == 0) {
-	        		
+	        		// This is an exception that the program handles
+	        		// If the user puts 0 as input the program can skip to the end
 	        		fbnSequence = ("0 is in the Fibonacci sequence\n"
 		    	    		+ "0 it's in position 1 of the Fibonacci sequence\n\n"
 		    	    		+ "The Fibonacci sequence up to " + N + " (or closest to):\n");
@@ -135,7 +145,8 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	        	}
 	        	
 	        	else if (N == 1) {
-	        		
+	        		// This is another exception that the program handles
+	        		// If the user puts 1 as input the program can skip to the end
 	        		fbnSequence = ("1 is in the Fibonacci sequence\n"
 		    	    		+ "1 it's in position 2 and 3 of the Fibonacci sequence\n\n"
 		    	    		+ "The Fibonacci sequence up to " + N + " (or closest to):\n");
@@ -144,38 +155,47 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	        		
 	        	}
 	        	
-	        	else {
-	        		
+	        	else {	
+	        		// 7540113804746346429L is the max value that the program can handle with longs
+	        		// and will be treated later on the program
+	        		// The while runs until reachs the value chosen by the user
 	        		while (last + sec <= N && last + sec < 7540113804746346429L) {
+	        			// Basic Fibonacci algorithm
 	        			next = last + sec;
 	        			sec = last;
 	        			last = next;
-	        			tempFbn += ", " + next;	  
-	        			pos++;	        			
+	        			tempFbn += ", " + next; // Stores the Fibonacci sequence 
+	        			pos++; // Stores the position	        			
 	        		}	      
 	        		
 	        		if ((last + sec) - N < N - last && N != 7540113804746346429L) {
-	        			
+	        			// If the value chosen by the user is closer to the next value
+	        			// of the Fibonacci sequence, the next value is stored
 	        			tempFbn += ", " + (last + sec);
-	        			
+	        			// Stores the text indicating that the value is not in the Fibonnaci sequence
 	        			fbnSequence = (N + " is not in the Fibonacci sequence\n\n"
 			    	    		+ "The Fibonacci sequence up to the value closest to " + N + ":\n\n");
 	        			
 	        		}	        		
 	        		else if (last == N) {
-	        		
+	        			// Stores the text indicating that the value is in the Fibonnaci sequence
 	        			fbnSequence = (N + " is in the Fibonacci sequence\n"
 			    	    		+ N + " it's in position " + pos + " of the Fibonacci sequence\n\n"
 			    	    		+ "The Fibonacci sequence up to " + N + ":\n\n");	
 	        			
 	        		}
 	        		else if (N == 7540113804746346429L) {
+	        			// Adds 7540113804746346429 to the sequence
+	        			// This stores the last possible value with longs
+	        			// ands prevents bugs (negatives numbers in the sequence, strange numbers, and so on)
 	        			tempFbn += ", " + (last + sec);
+	        			// Stores the text indicating that 7540113804746346429 is in the Fibonnaci sequence
 	        			fbnSequence = (N + " is in the Fibonacci sequence\n"
 			    	    		+ N + " it's in position " + (pos+1) + " of the Fibonacci sequence\n\n"
 			    	    		+ "The Fibonacci sequence up to " + N + ":\n\n");
 	        		}
 	        		else {
+	        			// Stores the text indicating that the value is not in the Fibonnaci sequence
 	        			fbnSequence = (N + " is not in the Fibonacci sequence\n\n"
 			    	    		+ "The Fibonacci sequence up to the value closest to " + N + ":\n\n");
 	        		}
@@ -184,7 +204,7 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	        		
 	        	}
 	        	        		
-        		fbnTextField.setText(fbnSequence);
+        		fbnTextField.setText(fbnSequence); // Puts the text in the text box for the user
 	        	
 	        }	        
 	        
