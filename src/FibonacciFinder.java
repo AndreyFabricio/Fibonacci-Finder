1e7686fc09238ccb157340b7890537aa5169eb7c
Andrey Fabricio <andrey.fas@gmail.com>
2022-01-22 13:06:37 -0300

    General improvements
    ## General improvements
    - Improvements in program reliability
    - Added a text indicating the position of the Nth value in the Fibonacci sequence
    - Added a text indicating if the number is not in the Fibonacci sequence
    - Changed from Integer to Long, improving how many Fibonacci sequence places the program can generate (now up to 93)

--- src/FibonacciFinder.java
+++ src/FibonacciFinder.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
  * This project generates the Fibonacci sequence to a number or to the closest Nth number in the sequence
  * Fibonacci sequence: https://en.wikipedia.org/wiki/Fibonacci_number
  * 
- * Last update: 21/01/2022
+ * Last update: 22/01/2022
  */
 
 public class FibonacciFinder extends JFrame implements ActionListener {
@@ -20,7 +20,7 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 		Container container=getContentPane();
 	    JLabel N = new JLabel("You want to generate the Fibonacci sequence up to what number?");
 	    JLabel error = new JLabel("");
-	    JLabel fbnLabel = new JLabel("");
+	    JLabel limit = new JLabel("Max value: 7540113804746346429");
 	    JTextField userTextField = new JTextField();
 	    JTextArea fbnTextField = new JTextArea();
 	    JButton btn = new JButton("Generate");
@@ -46,16 +46,16 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 		{
 			// Bounding the location and size of the objects in the container
 			N.setBounds(60,10,400,40);
-		    userTextField.setBounds(170,41,60,20);
-		    fbnTextField.setBounds(65,100,360,80);
+			limit.setBounds(150,30,200,40);
+		    userTextField.setBounds(130,61,140,20);
+		    fbnTextField.setBounds(65,90,360,430);
 		    fbnTextField.setLineWrap(true);
-		    fbnLabel.setBounds(60,70,400,40);
-		    btn.setBounds(230,40,90,20);
-		    error.setBounds(60,50,260,40);
-		    // Setting the color of the error label
-		    error.setForeground(new java.awt.Color(255,0,0));
 		    fbnTextField.setVisible(false);
 		    fbnTextField.setEditable(false);
+		    btn.setBounds(270,60,90,20);
+		    error.setBounds(60,70,400,40);
+		    // Setting the color of the error label
+		    error.setForeground(new java.awt.Color(255,0,0));
 		}
 		
 		public void addComponentsToContainer()
@@ -67,7 +67,7 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 		    container.add(fbnTextField);
 		    container.add(btn);
 		    container.add(error);
-		    container.add(fbnLabel);
+		    container.add(limit);
 		    
 		}
 		
@@ -91,7 +91,6 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	    	if (e.getSource() == btn || e.getSource() == userTextField) {
 	        	error.setText("");
 	        	fbnTextField.setVisible(false);
-	        	fbnLabel.setText(""); // Resets the label
 	        	createFibonacci();
 	        	userTextField.setText(""); // Resets the text field
 	        }
@@ -99,53 +98,89 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 	    }
 	
 	    private void createFibonacci() {
-
-	    	int N, sec, last, next;
+    		
+    		int pos = 2;
+	    	long N, sec, last, next;
 	    	boolean validInput = true;
 	    	
 	    	N = sec = next = 0;
 	    	last = 1;
 			
-	    	String fbnSequence = "0, 1";
+	    	String tempFbn = "0, 1";
+	    	String fbnSequence = "";
 	    	
 	        try {
-	            N = Integer.parseInt(userTextField.getText());
+	            N = Long.parseLong(userTextField.getText());
 	        } catch(NumberFormatException nfe) {        	
-	        	error.setText("The value \"" + userTextField.getText() +"\" is not a valid integer");
+	        	error.setText("The value \"" + userTextField.getText() +"\" is not valid");
 	        	validInput = false;
 	        }
 	        
 	        if (N < 0) {
 	        	error.setText("The value \"" + userTextField.getText() +"\" "
-	        			+ "is not a valid positive integer");
+	        			+ "is not a valid positive number");
 	        	validInput = false;
 	        }
 	        
 	        if (validInput) {
-	        	fbnLabel.setText("The Fibonacci sequence up to " + N + " (or closest to):");
 	        	fbnTextField.setVisible(true);
 	        	
 	        	if (N == 0) {
-	        		fbnSequence = "0";
+	        		
+	        		fbnSequence = ("0 is in the Fibonacci sequence\n"
+		    	    		+ "0 it's in position 1 of the Fibonacci sequence\n\n"
+		    	    		+ "The Fibonacci sequence up to " + N + " (or closest to):\n");
+	        		
+	        		fbnSequence += "0";
 	        	}
+	        	
+	        	else if (N == 1) {
+	        		
+	        		fbnSequence = ("1 is in the Fibonacci sequence\n"
+		    	    		+ "1 it's in position 2 and 3 of the Fibonacci sequence\n\n"
+		    	    		+ "The Fibonacci sequence up to " + N + " (or closest to):\n");
+	        		
+	        		fbnSequence += "0, 1, 1";
+	        		
+	        	}
+	        	
 	        	else {
 	        		
-	        		while (last + sec <= N) {
-	        			// TODO: proximity (Ex: 12 should print up to 13, but 10 should print up to 8)
+	        		while (last + sec <= N && last + sec < 7540113804746346429L) {
 	        			next = last + sec;
 	        			sec = last;
 	        			last = next;
-	        			fbnSequence += ", " + next;	        			
-	        			
-	        			// N = 7 | last = 5 | sec = 3
-	        			// se last+sec-N < N-last	        			
+	        			tempFbn += ", " + next;	  
+	        			pos++;	        			
 	        		}	      
 	        		
-	        		if ((last + sec) - N < N - last) {
+	        		if ((last + sec) - N < N - last && N != 7540113804746346429L) {
 	        			
-	        			fbnSequence += ", " + (last + sec);
+	        			tempFbn += ", " + (last + sec);
+	        			
+	        			fbnSequence = (N + " is not in the Fibonacci sequence\n\n"
+			    	    		+ "The Fibonacci sequence up to the value closest to " + N + ":\n\n");
+	        			
+	        		}	        		
+	        		else if (last == N) {
+	        		
+	        			fbnSequence = (N + " is in the Fibonacci sequence\n"
+			    	    		+ N + " it's in position " + pos + " of the Fibonacci sequence\n\n"
+			    	    		+ "The Fibonacci sequence up to " + N + ":\n\n");	
 	        			
 	        		}
+	        		else if (N == 7540113804746346429L) {
+	        			tempFbn += ", " + (last + sec);
+	        			fbnSequence = (N + " is in the Fibonacci sequence\n"
+			    	    		+ N + " it's in position " + (pos+1) + " of the Fibonacci sequence\n\n"
+			    	    		+ "The Fibonacci sequence up to " + N + ":\n\n");
+	        		}
+	        		else {
+	        			fbnSequence = (N + " is not in the Fibonacci sequence\n\n"
+			    	    		+ "The Fibonacci sequence up to the value closest to " + N + ":\n\n");
+	        		}
+	        		
+	        		fbnSequence += tempFbn;
 	        		
 	        	}
 	        	        		
@@ -160,7 +195,7 @@ public class FibonacciFinder extends JFrame implements ActionListener {
 		FibonacciFinder frame=new FibonacciFinder(); // Creates a new frame
         frame.setTitle("NthPI"); // Sets the title
         frame.setVisible(true); // Makes the frame visible
-        frame.setBounds(10,10,500,250); // Defines the bounds of the frame
+        frame.setBounds(10,10,500,600); // Defines the bounds of the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Programs closes when exits
         frame.setResizable(false); // And the program its not resizable
 
